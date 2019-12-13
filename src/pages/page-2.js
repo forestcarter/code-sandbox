5a724e1fac4f5e01fa314666afbf5e9042486cca
Forest Carter <forest.carter@gmail.com>
2019-12-12 22:04:12 -0500

    Began linked list

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,6 +1,6 @@
 import React from "react"
-import { Link } from "gatsby"
 import SSL from "../components/linkedList/SLL"
+
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
