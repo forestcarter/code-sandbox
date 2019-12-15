abbbdb459ff1d76f66984fad78f9b20c83616dde
Forest Carter <forest.carter@gmail.com>
2019-12-15 13:23:07 -0500

    Changed merge fail with index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,14 +5,11 @@ import SEO from "../components/seo"
 import Home from "../components/home/Home"
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great with changes Auto-deploy? Let's try.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <SEO
+      title="Home"
+      keywords={[`gatsby`, `react`, `bootstrap`, `Forest Carter`, `portfolio`]}
+    />
+    <Home />
   </Layout>
 )
 
